fdbc0d07cbf8ef179b3294f61d7594a5fa118a89
Celmar Pereira <celmarpereiradeandrade@gmail.com>
2025-10-31 14:09:37 -0300

    Adiciona o arquivo escola.sql

---
+++ escola.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Linux (x86_64)
+--
+-- Host: localhost    Database: Escola
+-- ------------------------------------------------------
+-- Server version	8.0.43-0ubuntu0.24.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Disciplina`
+--
+
+DROP TABLE IF EXISTS `Disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Disciplina` (
+  `RegistroDisciplina` int NOT NULL,
+  `RegistroProfessor` int NOT NULL,
+  `Descrição` varchar(50) DEFAULT NULL,
+  `DataCriação` date DEFAULT NULL,
+  `Horário` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`RegistroDisciplina`),
+  KEY `fk_PesCarro` (`RegistroProfessor`),
+  CONSTRAINT `fk_PesCarro` FOREIGN KEY (`RegistroProfessor`) REFERENCES `Professor` (`RegistroProfessor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Disciplina`
+--
+
+LOCK TABLES `Disciplina` WRITE;
+/*!40000 ALTER TABLE `Disciplina` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Disciplina` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Estudante`
+--
+
+DROP TABLE IF EXISTS `Estudante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Estudante` (
+  `RegistroAluno` int NOT NULL,
+  `NomeCompleto` varchar(50) DEFAULT NULL,
+  `Endereço` varchar(100) DEFAULT NULL,
+  `Cidade` varchar(50) DEFAULT NULL,
+  `DataMatricula` date DEFAULT NULL,
+  `NomePai` varchar(50) DEFAULT NULL,
+  `NomeMãe` varchar(50) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`RegistroAluno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Estudante`
+--
+
+LOCK TABLES `Estudante` WRITE;
+/*!40000 ALTER TABLE `Estudante` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Estudante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Professor`
+--
+
+DROP TABLE IF EXISTS `Professor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Professor` (
+  `RegistroProfessor` int NOT NULL,
+  `NomeCompletoProfessor` varchar(50) DEFAULT NULL,
+  `Endereço` varchar(100) DEFAULT NULL,
+  `Cidade` varchar(50) DEFAULT NULL,
+  `DataAdmissão` date DEFAULT NULL,
+  PRIMARY KEY (`RegistroProfessor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Professor`
+--
+
+LOCK TABLES `Professor` WRITE;
+/*!40000 ALTER TABLE `Professor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Professor` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-31 14:08:40
